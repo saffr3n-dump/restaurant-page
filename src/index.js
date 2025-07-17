8edce29c6cd2ceaf28aa8099f0ee80d2b48483df
saffr3n <saffr3n@gmail.com>
2025-07-17 20:56:48 +0300

    feat: add tabbed navigation logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import home from './home';
+import menu from './menu';
+import contact from './contact';
 
 const content = document.querySelector('.content');
 content.appendChild(home);
+
+const nav = document.querySelector('nav');
+nav.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'BUTTON') return;
+  content.innerHTML = '';
+
+  switch (e.target.textContent) {
+    case 'Home'   : return content.appendChild(home);
+    case 'Menu'   : return content.appendChild(menu);
+    case 'Contact': return content.appendChild(contact)
+  }
+});
